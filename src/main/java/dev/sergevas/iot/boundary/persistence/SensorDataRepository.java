f4b407510c43b82fc7dd7e38880a25334dcee8af
sergevas <brighterr@gmail.com>
2026-01-22 16:50:23 +0300

    In progress

---
+++ src/main/java/dev/sergevas/iot/boundary/persistence/SensorDataRepository.java
@@ -0,0 +1,30 @@
+package dev.sergevas.iot.boundary.persistence;
+
+import dev.sergevas.iot.entity.SensorDataEntity;
+import dev.sergevas.iot.entity.SensorNodeInfoEntity;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class SensorDataRepository {
+
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public void save(SensorDataEntity entity) {
+        em.persist(entity);
+    }
+
+    @Transactional
+    public Optional<SensorNodeInfoEntity> findByMacAddress(String macAddress) {
+        return em.createNamedQuery("SensorNodeInfoEntity.findByMacAddress", SensorNodeInfoEntity.class)
+                .setParameter("macAddress", macAddress)
+                .getResultStream()
+                .findFirst();
+    }
+}
